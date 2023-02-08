af8045f2737e15d67bb568175ae0550756f42076
Pankaj2130 <pankajvishwakarma23002@gamol.com>
2023-02-08 17:52:48 +0530

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [ MongooseModule.forRoot('mongodb+srv://Ritesh:UtbWeF8mXqnloNH0@cluster0.k6xufpn.mongodb.net/?retryWrites=true&w=majority'), UserModule, AuthModule],
+  imports: [ 
+    // ConfigModule.forRoot(),
+    MongooseModule.forRoot('mongodb+srv://Api:LoginAuth@cluster0.l4sf0uk.mongodb.net/?retryWrites=true&w=majority'), UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/appuser.schema.ts
+++ src/appuser.schema.ts
@@ -7,7 +7,7 @@ export const UserSchema=new mongoose.Schema({
 
 })
 
-UserSchema.pre('save', async function(next: mongoose.HookNextFunction) {
+UserSchema.pre('save', async function(next) {
     try {
       if (!this.isModified('password')) {
         return next();
